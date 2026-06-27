b7329e19f856835430d5f7fe928151f7d49a4c10
Octavianus <oktagandajaya@gmail.com>
2026-06-27 12:22:58 +0700

    Initial Commit

---
+++ IC-LMS/IC_LMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  IC_LMSApp.swift
+//  IC-LMS
+//
+//  Created by octavianus on 27/06/26.
+//
+
+import SwiftUI
+
+@main
+struct IC_LMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ IC-LMSTests/IC_LMSTests.swift
@@ -0,0 +1,19 @@
+//
+//  IC_LMSTests.swift
+//  IC-LMSTests
+//
+//  Created by octavianus on 27/06/26.
+//
+
+import Testing
+@testable import IC_LMS
+
+struct IC_LMSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
